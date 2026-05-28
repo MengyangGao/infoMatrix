f398afcba8ff2bdab7e41ef4410aae33feaec4a1
Mengyang Gao <gmy.kevinstark@gmail.com>
2026-05-28 20:42:36 +0800

    chore: bump version to 0.1.4

--- Casks/infomatrix.rb
+++ Casks/infomatrix.rb
@@ -1,7 +1,7 @@
 cask "infomatrix" do
   desc "Privacy-respecting cross-platform RSS reader"
   homepage "https://github.com/MengyangGao/infoMatrix"
-  version "0.1.1"
+  version "0.1.4"
   sha256 "257e62b48770743468a71b1808a5c4accec89a8377b753fdd63b96f204ced658"
 
   url "https://github.com/MengyangGao/infoMatrix/releases/download/v#{version}/InfoMatrix-macos.zip"
